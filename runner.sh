224e7fc756b6455f4439bd221a16ff1e09c045c2
Adi Zafri <adizafri2000@gmail.com>
2023-07-19 23:40:01 +0800

    updated python-app.yml and runner.sh for github actions

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-#xvfb-run java -Dwebdriver.chrome.driver=chromedriver/chromedriver_linux64_114-0-5735-16 -jar chromedriver/selenium-server-standalone.jar
+xvfb-run java -Dwebdriver.chrome.driver=chromedriver/chromedriver_linux64_114-0-5735-16 -jar chromedriver/selenium-server-standalone.jar
 
 echo "(1/5) Activating venv"
 . venv/bin/activate
