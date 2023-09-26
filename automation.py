1df66bea3bb656c1235472ce136ae19607ff3d20
Adi Zafri <adizafri2000@gmail.com>
2023-09-26 18:48:57 +0800

    update

--- automation.py
+++ automation.py
@@ -42,7 +42,7 @@ def generate_scshot_name(bill_type):
 def generate_screenshot(driver, bill_type):
     img_name = generate_scshot_name(bill_type)
     logger.info(f"Saving image to {img_name}")
-    res = driver.save_screenshot(img_name)
+    res = driver.get_screenshot_as_file(img_name)
     print(f"Is screenshot saved: {res}")
     storage.upload_to_bucket(folder=bill_type, file=img_name)
 
