c79a577920de1f5a9507e8634a3464f7c1aa3073
Adi Zafri <adizafri2000@gmail.com>
2025-01-01 22:16:07 +0800

    updated ws api req body

--- whatsapp.py
+++ whatsapp.py
@@ -13,8 +13,7 @@ ws_api_id = os.environ["ws_api_id"]
 def send_whatsapp_message(content: str):
     """Sends a message to the WhatsApp API"""
     params = {
-        "number": ws_api_id,
-        "content": content
+        "message": content
     }
     headers = {
         "accept": "application/json",
